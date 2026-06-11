6c98f8777bb3d21f59710b0a6678ae6ca7e7e1ce
Zac Vohs <ZacVohs-Consulting@pm.me>
2026-06-11 15:31:02 -0600

    Simplify

--- src/webroot/api/routes/api.routes.js
+++ src/webroot/api/routes/api.routes.js
@@ -3,7 +3,6 @@ import express from 'express';
 import { body } from 'express-validator';
 
 import { ROLES } from '../config/authServer.js';
-import { SERVER_CONFIG } from '../config/serverEnv.js';
 import { authorize } from '../middleware/login.middleware.js';
 
 import * as contactController from '../controllers/contact.controller.js';
@@ -15,19 +14,19 @@ router.get('/status', (req, res) => {
     res.json({ status: 'ok' });
 });
 
-router.post(`${SERVER_CONFIG.CONTEXT_ROOT}/api/contact`, [
+router.post(`/api/contact`, [
     body('from').notEmpty(),
     body('sender_email').isEmail(),
     body('subject').notEmpty(),
     body('body').notEmpty(),
 ], contactController.submitContactForm);
 
-router.get(`${SERVER_CONFIG.CONTEXT_ROOT}/api/emails`,
+router.get(`/api/emails`,
     authorize(ROLES.MANAGER),
     emailController.getEmails
 );
 
-router.post(`${SERVER_CONFIG.CONTEXT_ROOT}/api/emails`, [
+router.post(`/api/emails`, [
     body('email').isEmail(),
     body('action').isIn(['add', 'remove']),
 ], emailController.manageEmails);
